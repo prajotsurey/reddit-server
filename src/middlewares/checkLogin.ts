617c44d25417ab8084e1a8047d36bad8cbbb8017
prajotsurey <prajotsurey@gmail.com>
2022-01-21 17:21:07 +0530

    comments

--- src/middlewares/checkLogin.ts
+++ src/middlewares/checkLogin.ts
@@ -9,9 +9,20 @@ export const CheckLogin: MiddlewareFn<MyContext> = async ({ context }, next) => 
   if(!authorization) {
     return next()
   }
-
-  const payload = verify(authorization.split(' ')[1], process.env.ACCESS_TOKEN_SECRET!)
-  context.payload = payload as myPayload
-  return next()
+  try {
+    const payload = verify(authorization.split(' ')[1], process.env.ACCESS_TOKEN_SECRET!)
+    context.payload = payload as myPayload
+    return next()
+  } catch {
+    return next()
+  }
   
+  /*
+
+  Instead of throwing an error on invalid or missing token, we now do not set the payload object, thus keeping it undefined.
+  The resolvers can choose to throw error inside their own code if they want to.
+  This is done because have resolvers who work both with and without logged in user. i.e. with and without token in request.
+  Throwing error here wouldn't work for these resolvers beacuse it would stop execution.
+  Now these resolvers can just do 'not logged in' in execution instead. 
+  */
 }
